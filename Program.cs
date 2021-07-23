885981896c82c11035a89a5bde56d50f6fe7f367
Hellifax <hellifax91@gmail.com>
2021-07-23 10:15:52 +0300

    add task

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DB7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ход коня:");
+            new Horse(size: 5, startX: 0, startY: 0).Start();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Ферзи:");
+            new Elizabet2(8).Start();
+
+        }
+    }
+}
